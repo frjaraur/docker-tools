42bdc96d9f4cf628023c9d1d0db035178bf04761
Javier Ramirez <frjaraur@gmail.com>
2016-05-23 16:52:07 +0200

    Create simpleapp.js

---
+++ simpleapp.js
@@ -0,0 +1,12 @@
+var args = process.argv.slice(2);
+var http=require('http');
+var myip=require('my-local-ip')()
+var hostname=require('os').hostname();
+var port=args[0];
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(myip+' - '+hostname);
+}).listen(port);
+
+console.log(myip+' - '+hostname)
+console.log('Server running at http://localhost:'+port+'/');
