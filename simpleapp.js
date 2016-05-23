a6f923c96d624dc670a8bc56f6156b2cb7b9e10d
Javier Ramirez <frjaraur@gmail.com>
2016-05-23 17:20:01 +0200

    Added logging of client Address
    Added logging of client Address

--- simpleapp.js
+++ simpleapp.js
@@ -3,10 +3,13 @@ var http=require('http');
 var myip=require('my-local-ip')()
 var hostname=require('os').hostname();
 var port=args[0];
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(myip+' - '+hostname);
+  console.log("Request received from " + req.connection.remoteAddress);
 }).listen(port);
 
 console.log(myip+' - '+hostname)
 console.log('Server running at http://localhost:'+port+'/');
+
